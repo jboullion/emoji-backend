92e7d702ce208c642f60f94673d316a8ea84c259
jboullion <jboullion83@gmail.com>
2022-03-26 23:29:09 -0500

    Moving to Heroku

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -1,6 +1,8 @@
 import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
+  PORT: Joi.number().default(3001),
+  STAGE: Joi.string().default('dev').required(),
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().default(5432).required(),
   DB_USERNAME: Joi.string().required(),
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger();
-  const port = 3001;
+  const port = process.env.PORT;
 
   const app = await NestFactory.create(AppModule);
 
